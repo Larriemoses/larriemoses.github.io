9a064402623888003f3ce9fe9919de0c3789e380
Larriemoses <80601652+Larriemoses@users.noreply.github.com>
2024-09-17 12:51:20 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Use HashRouter
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
 import Home from './pages/Home';
 import './App.css';
 import Navbar from './components/Navbar';
-
 import Footer from './components/Footer';
-import ProjectDisplay from './pages/ProjectDisplay'
-
+import ProjectDisplay from './pages/ProjectDisplay';
 
 function App() {
   return (
-    <div className='App'>
-     <Router>
+    <div className="App">
+      <Router>
         <Navbar />
         <Routes>
-          <Route path ="/" element={<Home />} />
-          <Route path ="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={< ProjectDisplay />}/>
-          <Route path ="/experience" element={<Experience />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectDisplay />} />
+          <Route path="/experience" element={<Experience />} />
         </Routes>
-        <Footer /> 
-     </Router>
+        <Footer />
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
