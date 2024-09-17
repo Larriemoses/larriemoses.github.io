b0ac2498f9816007fcb9bc7698654bb9b8d68b54
Larriemoses <80601652+Larriemoses@users.noreply.github.com>
2024-09-17 10:18:29 +0000

    Update ProjectList.jsx

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -1,7 +1,7 @@
 import Pic1 from '../assets/img1.jpg'
 import Pic2 from '../assets/Img2.jpg'
 import Pic3 from '../assets/Img3.jpg' 
-import Pic4 from '../assets/Img4.JPG'
+import Pic4 from '../assets/Img4.jpg'
 import Pic5 from '../assets/Img5.jpg'
 import Pic6 from '../assets/Img6.jpg' 
 
